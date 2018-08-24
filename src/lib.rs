71bc7955adeab7d69dcb714d5ad1698c4e14c40d
MoezGholami <Ali.Moez.Gholami@Gmail.com>
2018-08-24 05:01:39 -0500

    using ec abstraction from cryptography utils and removing visibility wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 
-extern crate cryptography_utils;
-extern crate paillier;
-
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
+
+extern crate cryptography_utils;
+extern crate paillier;
 
 pub mod protocols;
--- src/protocols/two_party_ecdsa/lindell_2017/mod.rs
+++ src/protocols/two_party_ecdsa/lindell_2017/mod.rs
@@ -1,6 +1,5 @@
 
 pub mod party_one;
 pub mod party_two;
-pub mod structs;
 
 mod test;
