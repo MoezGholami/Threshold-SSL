7b72313dab58cd140b9e10c10a23b20ca0346d80
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-05-02 21:09:12 -0500

    change ssl certificate subject line

--- engine/ecengine/run_ecengine.sh
+++ engine/ecengine/run_ecengine.sh
@@ -9,11 +9,12 @@ gcc $ossl_include -fPIC -o ecengine.o -c ecengine.c
 gcc $all_ossl_flags -shared -o ecengine.so ecengine.o
 openssl engine -t -c `pwd`/ecengine.so
 
+rm -f hashs.txt
+
 set +e
 
-frozen_time="2010-01-02 22:33:44"
 for i in $(seq 1 100);
 do
-    faketime "$frozen_time" openssl req -engine `pwd`/ecengine.so -x509 -new -key mozroot.key -sha256 -days 3660 -out mozrootca.crt -passin pass:moez -subj "/C=IR/ST=Tehran/L=Tehran/O=Moez Home/OU=Security Department/CN=moezhome.ir/emailAddress=a_moezz@moezhome.ir" -set_serial 0xdbb4cfd2b11b4926 -rand ecengine.c
+    openssl req -engine `pwd`/ecengine.so -x509 -new -key mozroot.key -sha256 -days 3660 -out mozrootca.crt -passin pass:moez -subj "/C=IR/ST=Tehran/L=Tehran/O=Moez Home/OU=Security Department/CN=moezhome.ir/emailAddress=a_moezz@moezhome.ir" -set_serial 0xdbb4cfd2b11b4926 #-rand ecengine.c
     cat digest_pipe >> hashs.txt;
 done
