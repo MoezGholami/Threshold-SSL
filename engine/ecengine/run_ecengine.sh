a12422e12fec758cb7589a9a8644a08885c3e664
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-05-02 21:43:38 -0500

    improve output of mozecengine run output

--- engine/ecengine/run_ecengine.sh
+++ engine/ecengine/run_ecengine.sh
@@ -11,10 +11,9 @@ openssl engine -t -c `pwd`/ecengine.so
 
 rm -f hashs.txt
 
-set +e
-
 for i in $(seq 1 100);
 do
     openssl req -engine `pwd`/ecengine.so -x509 -new -key mozroot.key -sha256 -days 3660 -out mozrootca.crt -passin pass:moez -subj "/C=IR/ST=Tehran/L=Tehran/O=Moez Home/OU=Security Department/CN=moezhome.ir/emailAddress=a_moezz@moezhome.ir" -set_serial 0xdbb4cfd2b11b4926 #-rand ecengine.c
+    echo "the certificate created with $? status."
     cat digest_pipe >> hashs.txt;
 done
