faf5aaf275cf187a65f21c2595bfc54fd5c5a50b
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-05-04 16:09:07 -0500

    save changes of create unauthorized root certificate script

--- root_setup/create_unauthorized_root_certificate.sh
+++ root_setup/create_unauthorized_root_certificate.sh
@@ -3,13 +3,15 @@
 #!/bin/bash
 
 . ../constants.sh
-phony_passphrase="dashfllhlhg"
+phony_passphrase="moez"
 pr_key_location="./temp_files/unauthorized_root_private.key"
+pu_key_location="./temp_files/unauthorized_root_public.key"
 unauthorized_crt_location="./temp_files/unauthorized_root_certificate.crt"
 
 set -e
 echo "Generating phony key on curve $CURV"
 openssl ecparam -genkey -name $CURV | openssl ec -aes-256-cbc -passout pass:$phony_passphrase -out $pr_key_location
+openssl ec -in $pr_key_location -passin pass:$phony_passphrase -pubout > $pu_key_location
 echo "Key is generated"
 
 openssl req -x509 -new -nodes -key $pr_key_location -sha256 -days 3660 -out $unauthorized_crt_location -passin pass:$phony_passphrase -subj "/C=IR/ST=Tehran/L=Tehran/O=Sparkling Network/OU=Security Department/CN=moezhome.ir/emailAddress=a_moezz@moezhome.ir"
