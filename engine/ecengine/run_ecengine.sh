b5cfdb7d9386b49c209c4d65f3b5ed7a7643f74f
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-05-04 14:18:21 -0500

    create multiple certificates using the ec engine

--- engine/ecengine/run_ecengine.sh
+++ engine/ecengine/run_ecengine.sh
@@ -11,9 +11,14 @@ openssl engine -t -c `pwd`/ecengine.so
 
 rm -f hashs.txt
 
+cert_num=0
 for i in $(seq 1 100);
 do
     openssl req -engine `pwd`/ecengine.so -x509 -new -key mozroot.key -sha256 -days 3660 -out mozrootca.crt -passin pass:moez -subj "/C=IR/ST=Tehran/L=Tehran/O=Moez Home/OU=Security Department/CN=moezhome.ir/emailAddress=a_moezz@moezhome.ir" -set_serial 0xdbb4cfd2b11b4926 #-rand ecengine.c
     echo "the certificate created with $? status."
     cat digest_pipe >> hashs.txt;
+    diff mozrootca.crt $cert_num.crt || cert_num=$(($cert_num+1)) && cp mozrootca.crt $cert_num.crt && sleep 10
 done
+
+# for checking the difference
+    # for i in $(seq 2 10); do vimdiff $(($i-1)).crt $i.crt; done
