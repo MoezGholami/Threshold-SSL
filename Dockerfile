a4bf71b98f5fd59e032a261120849db2d28ccb4e
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-06-23 14:19:34 -0500

    the tests run on docker but there are issues with the ecengine
    It is probably necessary to re-write the ecengine to make it more
    compatible with newer openssl engines

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM ubuntu:18.04
 RUN apt-get update && \
     apt-get upgrade -y && \
     apt-get install --no-install-recommends -y \
-        gcc g++ make autoconf automake apt-utils python3 curl wget zip unzip man ca-certificates
+        gcc g++ make autoconf automake apt-utils python3 curl wget zip unzip man ca-certificates vim
 
 RUN cd && wget https://github.com/openssl/openssl/archive/OpenSSL_1_0_2r.zip && unzip OpenSSL_1_0_2r.zip && \
     mv openssl-OpenSSL_1_0_2r openssl && rm OpenSSL_1_0_2r.zip
 RUN cd ~/openssl && ./config && make && make install && ln -sf /usr/local/ssl/bin/openssl `which openssl` && rm -rf ~/openssl
+RUN mkdir /root/lab
+WORKDIR /root/lab
---
+++ tests/root_crt_creation/validity/run.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+set -e
+cd $(dirname "$0")
+
+pwd
+ossl_include="-I/usr/local/ssl/include"
+ossl_flags="$ossl_include -L/usr/local/ssl/lib -lssl -lcrypto -ldl"
+c_codes_dir="../../../../c_codes"
+priv1="rootkey.key"
+priv2="pr2.key"
+pub1="pu1.key"
+pub2="pu2.key"
+ok_runtime_config="ok_runtime_config.json"
+ok_c_params="ok_c_params.txt"
+ok_crt="ok.crt"
+corrupt_runtime_config="corrupt_runtime_config.json"
+corrupt_c_params="corrupt_c_params.txt"
+corrupt_crt="corrupt.crt"
+params_python_script="../../../../helper_scripts/params_feeder_root_c_code.py"
+
+rm -rf temp_files && mkdir temp_files
+
+cd temp_files
+gcc $ossl_include -fPIC -o ecengine.o -c "$c_codes_dir"/ecengine.c
+gcc $ossl_flags -shared -o ecengine.so ecengine.o && rm ecengine.o && cp ecengine.so /tmp/ecengine.so
+gcc "$c_codes_dir"/stub_signer.c $ossl_flags -o stub_signer.out
+gcc "$c_codes_dir"/root_ca_generate_from_key.c "$c_codes_dir"/commons.c $ossl_flags -o root_ca_generate_from_key.out
+
+openssl ecparam -genkey -name secp384r1 > "$priv1" && openssl ec -in "$priv1" -pubout > "$pub1"
+openssl ecparam -genkey -name secp384r1 > "$priv2" && openssl ec -in "$priv2" -pubout > "$pub2" && rm -f "$priv2"
+
+cat <<EOT >> $ok_runtime_config
+{
+    "engine_path" :         "/tmp/ecengine.so",
+    "pubkey_path" :         "$pub1",
+    "output_cert_path" :    "./$ok_crt",
+    "c_params_file_path":   "$ok_c_params",
+    "load_ecengine" :       true,
+    "debug" :               true
+}
+EOT
+python3 "$params_python_script" ../root_cert_parameters.json $ok_runtime_config
+
+cat <<EOT >> $corrupt_runtime_config
+{
+    "engine_path" :         "/tmp/ecengine.so",
+    "pubkey_path" :         "$pub2",
+    "output_cert_path" :    "./$corrupt_crt",
+    "c_params_file_path":   "$corrupt_c_params",
+    "load_ecengine" :       true,
+    "debug" :               true
+}
+EOT
+python3 "$params_python_script" ../root_cert_parameters.json $corrupt_runtime_config
+
+rm -rf digest_pipe signature_pipe && mkfifo digest_pipe signature_pipe
+./stub_signer.out & ./root_ca_generate_from_key.out "$ok_c_params"
+rm -rf digest_pipe signature_pipe && mkfifo digest_pipe signature_pipe
+./stub_signer.out & ./root_ca_generate_from_key.out "$corrupt_c_params"
