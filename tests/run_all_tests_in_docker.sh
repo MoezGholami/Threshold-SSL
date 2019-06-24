9ebf5d0107970d7ccdb95221412b01373771f210
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-06-24 11:45:18 -0500

    add debug mode in tests

--- tests/run_all_tests_in_docker.sh
+++ tests/run_all_tests_in_docker.sh
@@ -3,5 +3,8 @@
 set -e
 cd $(dirname "$0")/..
 docker build . -t ossl:pre > /dev/null
-#docker run -v "$(pwd)":/root ossl:pre /root/tests/run_all_tests.sh
-docker run -v "$(pwd)":/root/lab -it ossl:pre /bin/bash
+if [ "$1" = "debug" ]; then
+    docker run -v "$(pwd)":/root/lab -it ossl:pre /bin/bash
+else
+    docker run -v "$(pwd)":/root ossl:pre /root/tests/run_all_tests.sh
+fi
