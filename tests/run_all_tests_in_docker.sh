28bfda0a07921d4d077fe9c5912b1cbe1b223fb8
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-06-23 21:04:59 -0500

    modify test script

--- tests/run_all_tests_in_docker.sh
+++ tests/run_all_tests_in_docker.sh
@@ -4,4 +4,4 @@ set -e
 cd $(dirname "$0")/..
 docker build . -t ossl:pre > /dev/null
 #docker run -v "$(pwd)":/root ossl:pre /root/tests/run_all_tests.sh
-docker run -v "$(pwd)":/root -it ossl:pre /bin/bash
+docker run -v "$(pwd)":/root/lab -it ossl:pre /bin/bash
