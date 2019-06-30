ff416a26629bbc3419992deffb9585ac1b3e10e7
MoezGholami <Ali.Moez.Gholami@GMail.COM>
2019-06-30 18:15:10 -0500

    remove legacy dead codes and scripts

--- random_scripts_and_other_files/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ossl_flags="-I/usr/local/Cellar/openssl/1.0.2r/include -L/usr/local/Cellar/openssl/1.0.2r/lib -lssl -lcrypto"
-
-set -e
-python params_feeder_root_c_code.py root_cert_parameters.json runtime.json
-gcc root_ca_creator.c $ossl_flags -o root_ca_creator.out && ./root_ca_creator.out
--- random_scripts_and_other_files/run_csr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ossl_flags="-I/usr/local/Cellar/openssl/1.0.2r/include -L/usr/local/Cellar/openssl/1.0.2r/lib -lssl -lcrypto"
-
-set -e
-
-gcc stub_signer.c $ossl_flags -o stub_signer.out || exit
-./stub_signer.out &
-gcc csr_signer.c commons.c $ossl_flags -o csr_signer.out && ./csr_signer.out client_certificate_parameters.txt
